390873602f867d65067eb30fcf89c5ae107e1df1
Christian Fasching <christian.fasching@pimcore.com>
2023-03-08 15:49:30 +0100

    added integration tests action

--- .github/scripts/03-run-tests.sh
+++ .github/scripts/03-run-tests.sh
@@ -4,7 +4,7 @@ set -ex
 docker run --network=k6-test-network --rm -i \
   -v `pwd`/repos/pimcore/saas-k6/tests:/tests \
   grafana/k6 run /tests/testNotes.js \
-  -e HOST=http://nginx:8088 \
+  -e HOST=http://nginx:80 \
   -e USER=admin -e PASSWORD=pimcore -e SKIP_BOOTSTRAP=0 -e SKIP_CLEANUP=0 -e SKIP_WRONG_LOGIN=0 \
   -q
 
