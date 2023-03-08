53021e2f8d6ce9fab43ee57fa068f06ca786e9ee
Christian Fasching <christian.fasching@pimcore.com>
2023-03-08 16:13:38 +0100

    added integration tests action

--- .github/scripts/02-install-pimcore.sh
+++ .github/scripts/02-install-pimcore.sh
@@ -16,6 +16,6 @@ docker compose exec -T \
 php vendor/bin/pimcore-install -n
 
 
-docker compose exec -T bin/console cache:clear
+docker compose exec -T php bin/console cache:clear
 
 sudo chown -R www-data .
