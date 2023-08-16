9e343a29902971fb2dce6c68f0f02382a7c200f3
Christian Fasching <christian.fasching@pimcore.com>
2023-08-16 14:45:08 +0200

    prepared for 2023.2

--- .github/files/bundles.php
+++ .github/files/bundles.php
@@ -49,7 +49,7 @@ return [
     \Pimcore\Bundle\PerspectiveEditorBundle\PimcorePerspectiveEditorBundle::class => ['all' => true],
 
     \Pimcore\Bundle\BackendPowerToolsBundle\PimcoreBackendPowerToolsBundle::class => ['all' => true],
-    \Pimcore\Bundle\DataHubWebhooksBundle\PimcoreDatahubWebhooksBundle::class => ['all' => true],
+    \Pimcore\Bundle\DataHubWebhooksBundle\PimcoreDataHubWebhooksBundle::class => ['all' => true],
     \Pimcore\Bundle\DataQualityManagementBundle\PimcoreDataQualityManagementBundle::class => ['all' => true],
 ];
 
