75ca192c43b15062ded39d1b8ee3670803ff673a
Christian Fasching <fashxp@users.noreply.github.com>
2023-12-05 16:12:03 +0100

    switch to private packagist

--- .github/scripts/01-setup-environment.sh
+++ .github/scripts/01-setup-environment.sh
@@ -43,8 +43,8 @@ docker compose up -d
 
 
 # add platform version
-docker compose exec -T -- php composer config --global --auth http-basic.enterprise.repo.pimcore.com token $1
-docker compose exec -T -- php composer config repositories.pimcore_enterprise composer https://enterprise.repo.pimcore.com/
+docker compose exec -T -- php composer config --global --auth http-basic.pimcore.repo.packagist.com token $1
+docker compose exec -T -- php composer config repositories.pimcore_enterprise composer https://pimcore.repo.packagist.com/github-actions/
 docker compose exec -T -- php composer config repositories.dev path "./platform-version"
 
 docker compose exec -T -- php composer config minimum-stability dev
