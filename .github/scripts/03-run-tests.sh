50fc07cf1d504ae6c9f09e5276430c60ede21c3b
JiaJia Ji <kingjia90@gmail.com>
2025-04-11 15:29:34 +0200

    verbose

--- .github/scripts/03-run-tests.sh
+++ .github/scripts/03-run-tests.sh
@@ -6,6 +6,6 @@ docker run --network=k6-test-network --rm -i \
   grafana/k6 run /tests/testTenant.js \
   -e HOST=http://nginx:80 \
   -e USER=admin -e PASSWORD=pimcore -e SKIP_BOOTSTRAP=0 -e SKIP_CLEANUP=0 -e SKIP_WRONG_LOGIN=0 \
-  -q
+  --verbose
 
 
