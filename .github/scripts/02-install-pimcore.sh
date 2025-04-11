1ebe80799ffd22edc65178c1867acc1727ae6625
Herbert Roth <herbert.roth@pimcore.com>
2025-04-11 10:15:59 +0200

    Disable installation of TranslationsProvider bundle

--- .github/scripts/02-install-pimcore.sh
+++ .github/scripts/02-install-pimcore.sh
@@ -23,7 +23,7 @@ sleep 2
 
 docker compose exec -T php bin/console cache:clear
 
-docker compose exec -T php bin/console pimcore:bundle:install PimcoreTranslationsProviderInterfaceBundle
+#docker compose exec -T php bin/console pimcore:bundle:install PimcoreTranslationsProviderInterfaceBundle
 docker compose exec -T php bin/console pimcore:bundle:install PimcorePersonalizationBundle
 docker compose exec -T php bin/console pimcore:bundle:install PimcoreGlossaryBundle
 docker compose exec -T php bin/console pimcore:bundle:install PimcoreSeoBundle
