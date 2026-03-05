2cfa318bcd5f3aa1cd0b7203e5d59937d94f3cca
JiaJia Ji <kingjia90@gmail.com>
2026-03-05 20:18:12 +0100

    Clean up bundles.php by removing unused bundles
    Removed commented-out bundles and unnecessary entries.

--- .github/files/bundles.php
+++ .github/files/bundles.php
@@ -1,22 +1,12 @@
 <?php
 
 return [
-//    \Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle::class => ['all' => true],
-    \Pimcore\Bundle\PersonalizationBundle\PimcorePersonalizationBundle::class => ['all' => true],
-    \Pimcore\Bundle\GlossaryBundle\PimcoreGlossaryBundle::class => ['all' => true],
-    \Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
-    \Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+
     \Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
-    \Pimcore\Bundle\GoogleMarketingBundle\PimcoreGoogleMarketingBundle::class => ['all' => true],
     \Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
     \Pimcore\Bundle\WebToPrintBundle\PimcoreWebToPrintBundle::class => ['all' => true],
-//    \Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
     \Pimcore\Bundle\StaticRoutesBundle\PimcoreStaticRoutesBundle::class => ['all' => true],
-    \Pimcore\Bundle\NewsletterBundle\PimcoreNewsletterBundle::class => ['all' => true],
-    \Pimcore\Bundle\WordExportBundle\PimcoreWordExportBundle::class => ['all' => true],
-    \Pimcore\Bundle\XliffBundle\PimcoreXliffBundle::class => ['all' => true],
-    \Pimcore\Bundle\FileExplorerBundle\PimcoreFileExplorerBundle::class => ['all' => true],
-    \Pimcore\Bundle\SystemInfoBundle\PimcoreSystemInfoBundle::class => ['all' => true],
+
 
     \Pimcore\Bundle\ObjectMergerBundle\ObjectMergerBundle::class => ['all' => true],
 
@@ -39,18 +29,16 @@ return [
     \Pimcore\TranslationsProviderInterfaceBundle\PimcoreTranslationsProviderInterfaceBundle::class => ['all' => true],
     \Pimcore\Bundle\ObjectMergerBundle\ObjectMergerBundle::class => ['all' => true],
     \CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
-    \OutputDataConfigToolkitBundle\OutputDataConfigToolkitBundle::class => ['all' => true],
+
     \Pimcore\Bundle\WebToPrintBundle\PimcoreWebToPrintBundle::class => ['all' => true],
     \Pimcore\Bundle\StaticRoutesBundle\PimcoreStaticRoutesBundle::class => ['all' => true],
-    \Web2PrintToolsBundle\Web2PrintToolsBundle::class => ['all' => true],
 
-    \Pimcore\Bundle\PerspectiveEditorBundle\PimcorePerspectiveEditorBundle::class => ['all' => true],
 
     \Pimcore\Bundle\BackendPowerToolsBundle\PimcoreBackendPowerToolsBundle::class => ['all' => true],
     \Pimcore\Bundle\DataHubWebhooksBundle\PimcoreDataHubWebhooksBundle::class => ['all' => true],
     \Pimcore\Bundle\DataQualityManagementBundle\PimcoreDataQualityManagementBundle::class => ['all' => true],
     \Pimcore\Bundle\WorkflowAutomationIntegrationBundle\PimcoreWorkflowAutomationIntegrationBundle::class => ['all' => true],
-    \Pimcore\Bundle\AdminUiClassicLightThemeBundle\PimcoreAdminUiClassicLightThemeBundle::class => ['all' => true],
+
     \Pimcore\Bundle\GenericDataIndexBundle\PimcoreGenericDataIndexBundle::class => ['all' => true],
     \Pimcore\Bundle\CopilotBundle\PimcoreCopilotBundle::class => ['all' => true],
 
