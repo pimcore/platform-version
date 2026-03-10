dfd4b25a8888ac17522bd9d1fe438fa64c45085e
Herbert Roth <herbert.roth@pimcore.com>
2026-03-10 16:30:25 +0100

    Remove unused configuration files and integration test workflow
    Remove unused configuration files and integration test workflow

--- .github/scripts/03-run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-docker run --network=k6-test-network --rm -i \
-  -v `pwd`/saas-k6/tests:/tests \
-  grafana/k6 run /tests/testTenant.js \
-  -e HOST=http://nginx:80 \
-  -e USER=admin -e PASSWORD=pimcore -e SKIP_BOOTSTRAP=0 -e SKIP_CLEANUP=0 -e SKIP_WRONG_LOGIN=0 \
-  -q
